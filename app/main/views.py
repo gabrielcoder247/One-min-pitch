921dd86e04f89a9c15be17296bb87c73a4669954
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-02 18:59:49 +0300

    add views.py functions

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import main
-from ..models import *
+from ..models import Category,Pitch,Review
+from .forms import PitchForm, ReviewForm
+from flask_login import login_required,current_user
 
 @main.route('/')
 def index():
@@ -8,19 +10,42 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    category = Category.get_categories()
+    title ='Home'
+    return render_template('index.html', title = title, category = category)
 
-    # Getting categories
-    
-    # pickupline = get_pickupline('pickup')
-    pickupline = PickUp_line.query.all()
-    # interview_pitch = get_interview('interview')
-    # comment_pitch = get_comment('comment')
-    # category_pitch = get_category('category')
-    # product_pitch = get_product('product')
-    # promotional_pitch = get_promotional('promotional')
 
-    title = 'Home - Welcome to The best one minute pitch'
 
-    # title = title, category = category_pitch,   
-    # pickup = pickupline, interview = interview_pitch, comment = comment_pitch)
-    return render_template('index.html', pickup = pickupline)
+@main.route('/category/<int:id>')
+def category(id):
+    '''
+    view category function that returns the pitches of that category
+    '''
+    category = Category.query.get(id)
+    title = f'{category.name} pitches'
+    pitch = Pitch.get_pitches(category.id)
+
+    return render_template('category.html', title = title, category = category, pitch = pitch)
+
+@main.route('/category/pitch/new/<int:id>', methods = ["GET", "POST"])
+@login_required
+def new_pitch(id):
+    '''
+    view category that returns a form to create a pitch
+    '''
+    form = PitchForm()
+    category = Category.query.filter_by(id = id).first()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+
+        # pitch instance
+        new_pitch = Pitch(category_id = category.id, title = title, post = post, user = current_user)
+
+         # save pitch
+        new_pitch.save_pitch()  
+        return redirect(url_for('.category', id = category.id))
+    title = f'{category.name} pitches'
+    return render_template('new_pitch.html', title = title, pitch_form = form, category = category)
+
+
